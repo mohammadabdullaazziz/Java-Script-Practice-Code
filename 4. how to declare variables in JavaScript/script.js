dbc2cf2bacb08579c42ac5d1a0311c0133fc3640
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-02-18 21:14:28 +0600

    how to declare variables in JavaScript

---
+++ 4. how to declare variables in JavaScript/script.js
@@ -0,0 +1,57 @@
+var name = "Abdullah Arman";
+// document.write(name);
+name = "Aziz </br>";
+var age = 29;
+document.write(name,age);
+
+var name;
+var country;
+var age;
+name = "Mohammad </br>"
+country = "Bangladesh </br>"
+age = 56;
+document.write(name,country,age);
+var name,country,age;
+
+
+// string number boolean typeof operators code
+
+var name = "Abdullah Arman";
+var age = 29;
+console.log(name,age);
+
+var name = typeof 'Abdullah "Arman';
+var age = typeof 29;
+console.log(name,age)
+
+var name = typeof "Ujjol";
+var age = typeof 28;
+console.log(name,age);
+
+var isSubs = typeof true;
+var isPassed = typeof false;
+console.log(isSubs,isPassed);
+
+// boolean data type...
+// false=0
+// true=1
+console.log(3>2);
+console.log(10<5);
+console.log(5<10);
+
+// again repeat how to declare variable with string number boolean
+
+var name = "Abdullah Arman";
+// console.log(name);
+name = "Arman Aziz";
+// console.log(name);
+name = "Rokshana Aziz"
+console.log(name);
+
+
+var x = 10;
+// console.log(x);
+x = 20;
+// console.log(x);
+x = 50;
+console.log(x);
