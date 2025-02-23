0a2163222e76de75792ef505068807fba5035fb7
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-02-23 20:48:53 +0600

    Arithmetic and assignment operator

---
+++ 8. Arithmetic and assignment operator/script.js
@@ -0,0 +1,58 @@
+// Arithmetic operator 
+
+
+// Addition (+)    
+// Subtraction (-) 
+// Multiplication (*) 
+// Division (/)
+// Modulus (%)
+// Exponent (**)
+// Increment ++
+// Decrement --
+
+// Increment 
+// ++ Abdullah (Pre-Increment)
+// Abdullah ++ (Post-Increment)
+
+// Decrement 
+// ++ Abdullah (Pre-Decrement)
+// Abdullah ++ (Post-Decrement)
+
+
+
+// Assignment operator
+// =,  +=, -=, *=, /=, %=; **=,
+
+
+// 
+
+// +=,
+
+// var a = 5;
+// a += 4;    a = a+4;
+// console.log(a);
+
+// var a = 5;
+// a += 4;   
+// console.log(a);
+
+// 
+
+// -=,
+
+// var b = 20;
+// b -= 5;    b = b-5
+// console.log(b);
+
+var x = 6;
+x += 3; //x = x + 3;
+console.log(x)
+
+var y = 1;
+y -= 4; //y = y -4;
+console.log(y);
+
+
+var z = 2;
+z *= 6; //z = z * 6;
+console.log(z);
