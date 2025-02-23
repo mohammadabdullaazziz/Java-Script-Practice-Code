2292a7bd746209ba2311487aeaa275e25a691d4e
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-02-23 16:50:27 +0600

    how to add or concatenate strings

---
+++ 6. how to add or concatenate strings/script.js
@@ -0,0 +1,16 @@
+document.write("Abdullah " + "Aziz ");
+// 
+var firstName = "Mohammad ";
+var lastName = "Abdullah ";
+var fullName = firstName + lastName;
+document.write(fullName);
+
+document.write("My name is " + fullName);
+// 
+
+var num1 = 20;
+var num2 =30;
+document.write("Num1 = " + num1 + " and Num2 = " + num2);
+
+
+
