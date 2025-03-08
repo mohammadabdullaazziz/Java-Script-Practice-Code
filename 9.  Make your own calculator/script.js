d5b0b60fcdf9901764fe42146a1ae77d06a61e97
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-03-08 21:12:39 +0600

    Make your own calculator

---
+++ 9.  Make your own calculator/script.js
@@ -0,0 +1,53 @@
+var num1 = 20;
+var num2 = 10;
+var sum,sub;
+
+sum = num1 + num2;
+document.write("Addition = " + sum + "</br>");
+
+sub = num1 - num2;
+document.write("Subtraction = " + sub + "</br>");
+
+// Input from the user
+
+var num1 = prompt("Enter First Number");
+var num2 = prompt("Enter Second Number");
+// string convert Integer
+num1 = parseInt(num1, 10);
+num2 = parseInt(num2, 10);
+var sum, sub;
+
+sum = num1 + num2;
+document.write("Addition = " + sum + "</br>");
+
+sub = num1 - num2;
+document.write("Subtraction = " + sub);
+
+
+
+var num1 = prompt("Enter First Number");
+var num2 = prompt("Enter Second Number");
+// string convert Integer
+num1 = parseInt(num1, 10);
+num2 = parseInt(num2, 10);
+var result;
+
+result = num1 + num2;
+document.write(num1 + " + " + num2 + " = " + result + "</br>");
+
+result = num1 - num2;
+document.write(num1 + " - " + num2 + " = " + result + "</br>");
+
+
+result = num1 * num2;
+document.write(num1 + " * " + num2 + " = " + result + "</br>");
+
+result = num1 / num2;
+document.write(num1 + " / " + num2 + " = " + result + "</br>");
+
+result = num1 % num2;
+document.write(num1 + " % " + num2 + " = " + result + "</br>");
+
+
+
+
