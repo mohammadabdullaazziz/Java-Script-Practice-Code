10525b1723c8b08fb5d474fcf9cbdba6c4740d70
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-03-17 22:24:38 +0600

    Relational and Logical Operator

---
+++ 12. Relational and Logical Operator/script.js
@@ -0,0 +1,50 @@
+// var num1 = 20;
+// var num2 = 25;
+// console.log(num1 > num2); false
+
+
+// var num1 = 20;
+// var num2 = 25;
+// console.log(num1 < num2); true
+
+
+// var num1 = 20;
+// var num2 = 25;
+// console.log(num1 >= num2); false
+
+// var num1 = 20;
+// var num2 = 20;
+// console.log(num1 <= num2); true
+
+
+// console.log(20==20); true
+// console.log(20==40); false
+
+// console.log(20=="20"); true
+
+
+// console.log(20==="20"); false
+// console.log(20===30); false
+
+
+// console.log(20!=30); true
+
+// console.log(25!=20); true
+// console.log(25!=25); false
+
+// logical operator
+// var num1 = 50;
+// var num2 = 30;
+// var num3 = 20;
+// console.log(num1 > num2 && num1 < num3); false
+
+// var num1 = 50;
+// var num2 = 30;
+// var num3 = 20;
+// console.log(num1 > num2 || num1 < num3); true
+
+// console.log(! true); 
+// console.log(! false); 
+
+console.log(! (28 > 25)); 
+console.log(! (28 < 25)); 
