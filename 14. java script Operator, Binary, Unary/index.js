b18e97a27f3ae407bd433a73df5fa7ac81d49dd3
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-05-07 11:23:15 -0700

    java script Operator, Binary, Unary

---
+++ 14. java script Operator, Binary, Unary/index.js
@@ -0,0 +1,37 @@
+let mangoPrice = 40;
+let applePrice = 20;
+console.log(mangoPrice + applePrice); addition + operator
+console.log(mangoPrice - applePrice);   subtraction - operator
+console.log(mangoPrice * applePrice);   multiplication * operator
+console.log(mangoPrice / applePrice);   division / operator
+console.log(mangoPrice % applePrice);   modulus % operator
+// ++ increment -- decrement 
+
+// ** exponent
+console.log(5 ** 2);
+
+// unary operator
+
+// let a = 50;
+// console.log(++a); ++ increment
+// console.log(--a); -- decrement
+// console.log(-a);  negation operator
+// console.log(a+);  negation operator
+
+
+let num = "100";
+console.log(typeof +num); Number 
+
+// Binary operator
+let mango = 40;
+let apple = 20;
+console.log(mango + apple); 
+console.log(mango - apple);   
+console.log(mango * apple);   
+console.log(mango / apple);   
+console.log(mango % apple); 
+
+let firstNumber = 8;
+let secondNumber = 2;
+let result = firstNumber ** secondNumber;
+console.log(result);
