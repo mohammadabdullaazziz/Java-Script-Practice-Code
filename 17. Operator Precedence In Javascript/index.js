f73107d9846b4c51c0503e409f67070694b0751d
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-05-11 08:14:56 -0700

    Operator Precedence In Javascript

---
+++ 17. Operator Precedence In Javascript/index.js
@@ -0,0 +1,34 @@
+let num = 2 + 3; // statement
+
+2 + 3 // complete Expression
+
+// Operator  Precedence
+console.log(2 + 3 * 4);  // 14
+
+let num2 = (10 + 5) * 2; // 30
+console.log(num2);
+
+let num3 = 10 / 5 * 2; // 4
+console.log(num3);
+
+let num4 = 3 * 2 + 4; // 10
+console.log(num4);
+
+let num5 = 3 + 2 - 4; // 1 same precedent (left to right)
+console.log(num5);
+
+let num6 = 3 * 2 / 3; // 2 same precedent (left to right)
+console.log(num6);
+
+let num7 = (3 + 2) * (5 + 2);  // 35
+console.log(num7);
+
+let num8 = 3 + 2 * 5 + 2;  // 15
+console.log(num8);
+
+let age1 = 20;
+let age2 = 30;
+let age = +age1 + +age2;
+// console.log(age); // 2030
+console.log(age); // 50
+
