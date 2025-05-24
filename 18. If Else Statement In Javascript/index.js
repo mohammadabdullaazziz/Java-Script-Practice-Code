a63e580118fe2aee652894eafee9ff2ec81a0030
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2025-05-24 12:05:02 -0700

    If Else Statement In Java script

---
+++ 18. If Else Statement In Javascript/index.js
@@ -0,0 +1,26 @@
+let myName = "Abdullah";
+if (myName == "Abdullah") {
+    console.log("Hello");
+}
+
+if (3 < 5) {
+    console.log("Yes True");
+}
+
+let name = "Aziz";
+if (name == "Abdullah") {
+    console.log("My Name");
+}
+
+if ("Abdullah") {
+    console.log("Yes True");
+}
+
+let a;
+if (a = "Abdullah") {
+    console.log("is True");
+}
+let b;
+if (b = null) {
+    console.log("is True");
+}
